9939aba14bb9d832ca2b1f4d197b498c36ad20e7
bigzero <bigzero389@gmail.com>
2022-12-11 10:57:01 +0900

    modify: by output

--- ch02/main.tf
+++ ch02/main.tf
@@ -5,9 +5,37 @@ provider "aws" {
 resource "aws_instance" "example" {
 	ami = "ami-0eddbd81024d3fbdd"
 	instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
 
   tags = {
     Name = "terraform-example-dyheo"
   }
 }
-	
+
+resource "aws_security_group" "instance" {
+  name = "dyheo-tf-instance"
+
+  ingress {
+    from_port = var.server_port
+    to_port = var.server_port
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP request"
+  type = number
+  default = 8080
+}
+
+output "public_ip" {
+  value = aws_instance.example.public_ip
+  description = "The public IP address of the web server"
+}
