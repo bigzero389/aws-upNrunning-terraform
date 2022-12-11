9772ce03cc10a2749adce268e66ba5efcd0f03bd
bigzero <bigzero389@gmail.com>
2022-12-11 17:07:27 +0900

    doing: ch02 ing

--- ch02/main.tf
+++ ch02/main.tf
@@ -14,12 +14,12 @@ resource "aws_instance" "example" {
               EOF
 
   tags = {
-    Name = "dyheo-tf"
+    Name = "dy-tf-instance"
   }
 }
 
 resource "aws_security_group" "instance" {
-  name = "dyheo-tf-instance"
+  name = "dy-tf-sg"
 
   ingress {
     from_port = var.server_port
@@ -39,3 +39,135 @@ output "public_ip" {
   value = aws_instance.example.public_ip
   description = "The public IP address of the web server"
 }
+
+# ASG setting
+resource "aws_launch_configuration" "example" {
+  image_id = "ami-0eddbd81024d3fbdd"
+  instance_type = "t3.micro"
+  security_groups = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier = data.aws_subnet_ids.default.ids
+
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key = "Name"
+    value = "dy-tf-asg-ex"
+    propagate_at_launch = true
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+
+# ALB setting
+resource "aws_lb" "example" {
+  name = "dy-tf-lb"
+  load_balancer_type = "application"
+  subnets = data.aws_subnet_ids.default.ids
+
+  security_groups = [aws_security_group.alb.id]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port = 80
+  protocol = "HTTP"
+
+  # default return is 404 page error 
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code = 404
+    }
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name = "dy-tf-alb"
+
+  # permission traffic
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_target_group" "asg" {
+  name = "dy-tf-asg"
+  port = var.server_port
+  protocol = "HTTP"
+  vpc_id = data.aws_vpc.default.id
+
+  health_check {
+    path = "/"
+    protocol = "HTTP"
+    matcher = "200"
+    interval = 15
+    timeout = 3
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
+output "alb_dns_name" {
+  value = aws_lb.example.dns_name 
+  description = "The domain name of the load balance"
+}
+
+
+
+
+
+
