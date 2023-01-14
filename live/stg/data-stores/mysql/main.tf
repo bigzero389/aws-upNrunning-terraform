b9455e59aeacafa17efbb206ea2f3e9e1c51d146
bigzero <bigzero389@gmail.com>
2023-01-14 17:13:04 +0900

    modify: add var environment into tf script in stage data-stores

---
+++ live/stg/data-stores/mysql/main.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+module "mysql" {
+  # source = "git@github.com:largezero/tf-upNrunning//modules/data-stores/mysql?ref=v0.0.2"
+  source = "../../../../modules/data-stores/mysql"
+
+  db_password = var.password
+  cluster_name = var.cluster_name # stg
+  environment = var.environment # stg
+  instance_type = "db.t2.micro"
+  admin_username = "admin"
+
+  # db_remote_state_bucket = "dy-tf-state"
+  # db_remote_state_key = "stage/data-stores/mysql/terraform.tfstate"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "dy-tf-state"
+    key = "stage/data-stores/mysql/terraform.tfstate" # stage
+    region = "ap-northeast-2"
+
+    dynamodb_table = "dy-tf-locks"
+    encrypt = true
+  }
+}
+
---
+++ live/stg/data-stores/mysql/variables.tf
@@ -0,0 +1,17 @@
+variable "password" {
+  description = "The password of the database. (e.g. gjeodud01)"
+  type = string
+}
+
+variable "cluster_name" {
+  description = "All my Project Name"
+  type = string
+  default = "dy-tf"
+}
+
+variable "environment" {
+  description = "System Object (e.g. dev stg prd)"
+  type = string
+  default = "stg"
+}
+
